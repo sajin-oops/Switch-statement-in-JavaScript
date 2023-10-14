c95bdf4bc1c6f13f3d9fb5cc5a0ee3b5b4d8d5f4
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2023-10-14 19:02:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// - switch statement in javascript
+
+let city = "Chennai"
+  switch(city){
+    case 'Bengaluru':
+      console.log("population = 8443675")
+      console.log("literacyRate = 88.71")
+      console.log("language = 'Kannada'")
+      break
+      case 'Chennai':
+      console.log("population = 4646732")
+      console.log("literacyRate = 90.20")
+      console.log("language = 'Tamil'")  
+        break
+  
+  }
